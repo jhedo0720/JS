e075e9d04b9944e40da2d4f1939dc3a2effe0794
J h a p <78457356+jhap20xx@users.noreply.github.com>
2024-09-19 10:40:35 +0800

    Type Conversions

---
+++ basic_JS/TypeConversions.js
@@ -0,0 +1,11 @@
+/* Type Conversion  = change of datatype of a value to another
+                        (strings, number, boolean)
+   -  Number()
+   -  String()
+   -  Boolean()
+*/
+let age = window.prompt("How old are you?"); // string data type
+age = Number(age) //type conversion / string to number
+age +=1
+
+console.log(age);
