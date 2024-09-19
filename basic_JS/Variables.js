ac6fb2e822bf0e877f5b856a38a936295adfefee
J h a p <78457356+jhap20xx@users.noreply.github.com>
2024-09-19 10:38:48 +0800

    Variables

---
+++ basic_JS/Variables.js
@@ -0,0 +1,10 @@
+let age = 24;
+let name = "Jack";
+let online = true;
+
+console.log(typeof name);
+console.log(`My name is ${name}`);
+console.log(typeof age);
+console.log(`My age is ${age}`);
+console.log(typeof online);
+console.log(`I am online: ${online}`);
