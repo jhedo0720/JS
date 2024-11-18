f3fef155c87ac9b7cfc2b9e289840eff150efc1a
J h a p <78457356+jhap20xx@users.noreply.github.com>
2024-11-18 11:46:11 +0800

    Logical Operators

---
+++ basic_JS/Logical_Operators.js
@@ -0,0 +1,21 @@
+// Logical Operators = used to combine or manipulate boolean values 
+                        //  (true or false)
+                        //  AND  = &&
+                        //  OR   = ||
+                        //  NOT  = !
+
+// const temp = -20;
+
+// if (temp > 0 && temp <= 30 ){
+//     console.log("The weather is Good");
+// } else {
+//     console.log("The weather is Bad");
+// }
+
+// const isSunny = false;
+
+// if(!isSunny){
+//     console.log("It is Cloudy");
+// }else{
+//     console.log("It is Sunny");
+// }
