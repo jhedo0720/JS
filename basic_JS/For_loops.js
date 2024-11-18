ba8775d03cbaa7dfa970e0da2bffaa55e8ca7fae
J h a p <78457356+jhap20xx@users.noreply.github.com>
2024-11-18 11:47:00 +0800

    For Loops

---
+++ basic_JS/For_loops.js
@@ -0,0 +1,11 @@
+//  for loops = repeat some code a LIMITED amount of times
+
+for(let i = 2; i <= 20; i+=2){
+    
+    // if(i == 13){
+    //     continue;
+    // }else{
+        console.log(i);
+    // }
+
+}
